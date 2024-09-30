21cc58ef88e2ecf4cad5cca03015a0a50372ebbd
EduardMi <54888468+LOLIPOPoSuge@users.noreply.github.com>
2024-09-30 19:39:56 +0300

    Add files via upload

---
+++ tesla_web/cdn3.optimizely.com/js/geo4.js
@@ -0,0 +1,17 @@
+(function() {
+    window['optimizely'] = window['optimizely'] || [];
+    window['optimizely'].push(['activateGeoDelayedExperiments', {
+        'location': {
+            'city': "CRAIOVA",
+            'continent': "EU",
+            'country': "RO",
+            'region': "DJ",
+            'dma': ""
+        },
+        'ip': "86.121.242.192"
+    }]);
+})
+
+()
+
+;
